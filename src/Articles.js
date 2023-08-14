611dae5ff452f9a23106bdbe72ddb69d7e7be918
Nishant-Gupta <gnishant@gmail.com>
2023-08-14 19:31:11 +0530

    fix issues

--- src/Articles.js
+++ src/Articles.js
@@ -14,7 +14,7 @@ import Loading from "./Loading";
 import { addAemHost } from "./api/aemHeadlessClient";
 
 //import backIcon from '../images/icon-close.svg';
-import { mapJsonRichText } from './utils/renderRichText';
+//import { mapJsonRichText } from './utils/renderRichText';
 //import './Article.scss';
 
 function Article() {
@@ -70,43 +70,43 @@ function ArticleListItem({ articleTitle, slug, teaserimage, summary, references 
 /**
  * Example of using a custom render for in-line references in a multi line field
  */
-function customRenderOptions(references) {
+// function customRenderOptions(references) {
 
-    console.log('references',references);
-    const renderReference = {
-        // node contains merged properties of the in-line reference and _references object
-        'ImageRef': (node) => {
-            // when __typename === ImageRef
-            return <img src={addAemHost(node._path)} alt={'in-line reference'} />
-        },
-        // 'AdventureModel': (node) => {
-        //     // when __typename === AdventureModel
-        //     return <Link to={`/article:${node.slug}`}>{`${node.title}: ${node.price}`}</Link>;
-        // }
-    };
+//     console.log('references',references);
+//     const renderReference = {
+//         // node contains merged properties of the in-line reference and _references object
+//         'ImageRef': (node) => {
+//             // when __typename === ImageRef
+//             return <img src={addAemHost(node._path)} alt={'in-line reference'} />
+//         },
+//         // 'AdventureModel': (node) => {
+//         //     // when __typename === AdventureModel
+//         //     return <Link to={`/article:${node.slug}`}>{`${node.title}: ${node.price}`}</Link>;
+//         // }
+//     };
 
-    return {
-        nodeMap: {
-            'reference': (node, children) => {
+//     return {
+//         nodeMap: {
+//             'reference': (node, children) => {
 
-                // variable for reference in _references object
-                let reference;
+//                 // variable for reference in _references object
+//                 let reference;
 
-                // asset reference
-                if (references && node.data.path) {
-                    // find reference based on path
-                    reference = references.find(ref => ref._path === node.data.path);
-                }
-                // Fragment Reference
-                if (references && node.data.href) {
-                    // find in-line reference within _references array based on href and _path properties
-                    reference = references.find(ref => ref._path === node.data.href);
-                }
+//                 // asset reference
+//                 if (references && node.data.path) {
+//                     // find reference based on path
+//                     reference = references.find(ref => ref._path === node.data.path);
+//                 }
+//                 // Fragment Reference
+//                 if (references && node.data.href) {
+//                     // find in-line reference within _references array based on href and _path properties
+//                     reference = references.find(ref => ref._path === node.data.href);
+//                 }
 
-                // if reference found return render method of it
-                return reference ? renderReference[reference.__typename]({ ...reference, ...node }) : null;
-            }
-        },
-    };
-}
+//                 // if reference found return render method of it
+//                 return reference ? renderReference[reference.__typename]({ ...reference, ...node }) : null;
+//             }
+//         },
+//     };
+// }
 export default Article;
